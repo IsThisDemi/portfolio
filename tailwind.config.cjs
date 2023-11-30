777ab4d3e51908a270a530154ea642a2c24a1e40
IsThisDemi <furryalt68@gmail.com>
2023-11-30 21:21:42 +0100

    restyle start

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,11 +7,37 @@ module.exports = {
   darkMode: ['class'],
   theme: {
     colors: {
+      waikawa: {
+        50: '#f2f7fb',
+        100: '#e7f0f8',
+        200: '#d3e2f2',
+        300: '#b9cfe8',
+        400: '#9cb6dd',
+        500: '#839dd1',
+        600: '#6a7fc1',
+        700: '#6374ae',
+        800: '#4a5989',
+        900: '#414e6e',
+        950: '#262c40',
+      },
       transparent: 'transparent',
       current: 'currentColor',
       white: colors.white,
-      primary: colors.indigo,
+      primary: colors.rose,
       gray: colors.gray,
+      // gray: {
+      //   50: '#f2f7fb',
+      //   100: '#e7f0f8',
+      //   200: '#d3e2f2',
+      //   300: '#b9cfe8',
+      //   400: '#9cb6dd',
+      //   500: '#839dd1',
+      //   600: '#6a7fc1',
+      //   700: '#6374ae',
+      //   800: '#4a5989',
+      //   900: '#414e6e',
+      //   950: '#262c40',
+      // },
     },
     extend: {
       spacing: {
